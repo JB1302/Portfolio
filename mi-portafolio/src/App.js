c608eee1a211f62c40108abbc9575c0a9e8614a2
JB1302 <jstevenbarrantes@gmail.com>
2025-03-28 00:50:44 -0600

    removido 100vw

--- mi-portafolio/src/App.js
+++ mi-portafolio/src/App.js
@@ -3,7 +3,7 @@ import Boilerplate from './components/boilerplate';
 function App() {
   console.log('El componente App se está ejecutando.');
   return (
-    <div style={{width: '100vw', height: '100vh'}}>
+    <div>
       <Boilerplate />
     </div>
   );
