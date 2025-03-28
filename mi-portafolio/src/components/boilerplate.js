19bb191ba17f8a119a03efc5ca9c1e8441b29b67
JB1302 <jstevenbarrantes@gmail.com>
2025-03-27 23:45:45 -0600

    Footer

--- mi-portafolio/src/components/boilerplate.js
+++ mi-portafolio/src/components/boilerplate.js
@@ -5,12 +5,28 @@ function Boilerplate() {
   console.log('El componente Boilerplate se está ejecutando.'); // Mensaje de depuración
   return (
     <section className="boilerplate">
-      <p>El componente Boilerplate se está renderizando.</p>
       <div className="header">1</div>
       <div className="leftSide">2</div>
       <div className="main">3</div>
       <div className="rightside">4</div>
-      <div className="footer">5</div>
+
+      <div className="footer">
+
+            <div className="footerLeft">
+              <p><strong>- Email:</strong> Jstevenbarrantes@gmail.com</p>
+              <p><strong>- GitHub:</strong> JB1302</p>
+              <p><strong>- LinkedIn:</strong> Steven Barrantes</p>
+            </div>
+
+            <div className="footerdivider"></div>
+       
+           <div className="footerRight">
+              <p>Feel free to reach out for collaboration or just to chat about development, data engineering, or tech in general.</p>
+           </div>
+
+          
+
+      </div>
     </section>
   );
 }
