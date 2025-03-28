2ec11f7a69169169ccec004bfab4f09f4d09a9ce
JB1302 <jstevenbarrantes@gmail.com>
2025-03-28 00:28:34 -0600

    boilerplate done

--- mi-portafolio/src/components/boilerplate.js
+++ mi-portafolio/src/components/boilerplate.js
@@ -5,10 +5,24 @@ function Boilerplate() {
   console.log('El componente Boilerplate se está ejecutando.'); // Mensaje de depuración
   return (
     <section className="boilerplate">
-      <div className="header">1</div>
-      <div className="leftSide">2</div>
-      <div className="main">3</div>
-      <div className="rightside">4</div>
+      <div className="header">
+        <div className="headerLeft">
+          <img src="/imagenes/data-engineer.png" alt="Data Engineer" />
+          <div className="tituloPagina">
+            <p><strong>Turning Data</strong></p>
+            <p><strong>Into Solutions</strong></p>
+          </div>
+        </div>
+        <div className="headerRight">
+           <button type="button" className="btn btn-primary">HOME</button>
+           <button type="button" className="btn btn-primary">CONTACT</button>
+           <button type="button" className="btn btn-primary">PROJECTS</button>
+        </div>  
+
+      </div>
+      <div className="leftSide"></div>
+      <div className="main"></div>
+      <div className="rightside"></div>
 
       <div className="footer">
 
